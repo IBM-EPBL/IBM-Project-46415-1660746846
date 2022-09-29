5e1de6d35eea09d733fec3643ee74cbc7c504d08
vaishu0723 <113779860+vaishu0723@users.noreply.github.com>
2022-09-29 14:28:57 +0530

    Add files via upload

---
+++ ASSIGNMENT OUTPUT.py
@@ -0,0 +1,11 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================================== RESTART: C:/Users/admin/AppData/Local/Programs/Python/Python310/Assignment-2(TEAM LEADER).py ==================================
+High temperature and humidity of: 66 82 % alarm is on
+High temperature and humidity of: 88 77 % alarm is on
+High temperature and humidity of: 63 63 % alarm is on
+High temperature and humidity of: 81 73 % alarm is on
+High temperature and humidity of: 64 64 % alarm is on
+High temperature and humidity of: 71 88 % alarm is on
+Normal temperature and humidity of: 17 52 % alarm is off
